1c9c5df975ffa140463af8621590d283c3a183af
Vulpes Foxnik <hiddenfoxling@gmail.com>
2018-06-02 13:20:17 -0400

    adding categories to items

--- plugins/etc/items.js
+++ plugins/etc/items.js
@@ -1,1 +1,18 @@
+const fs = require('fs');
+/**
+ * @type {Array<ItemSource>}
+ */
 module.exports = JSON.parse(fs.readFileSync("./data/items.json"))||[];
+/**
+ * @typedef {('garment'|'weapon'|'armor'|'item'|'unclassified')} ItemType
+ */
+
+/**
+ * @typedef {object} ItemSource
+ * @param  {number} id
+ * @param {string} title
+ * @param {number} XP
+ * @param {number} Gold
+ * @param {boolean} hidden
+ * @param {ItemType} type
+ */
