ea49422a6526c8d1e7694ce8c5ca5cd2ff1beb91
Vulpes Foxnik <hiddenfoxling@gmail.com>
2018-06-02 14:26:22 -0400

    Correcting Title Number Sources, added hidden property to tall Title Sources

--- plugins/etc/titles.js
+++ plugins/etc/titles.js
@@ -1,1 +1,14 @@
-module.exports = JSON.parse(fs.readFileSync("./data/items.json")) ||[];
+const fs = require('fs');
+/**
+ * @type {Array<TitleSource>}
+ */
+module.exports = JSON.parse(fs.readFileSync("./data/titles.json")) ||[];
+
+/**
+ * @typedef {object} TitleSource
+ * @param  {number} id
+ * @param {string} title
+ * @param {number} XP
+ * @param {number} Gold
+ * @param {boolean} hidden
+ */
