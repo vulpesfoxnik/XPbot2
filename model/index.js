c8d6d4790b4cc04ad8f6405d82f0ead012953b90
SweetsBot <android5789+sweetsbot@gmail.com>
2018-07-06 00:00:21 -0400

    Database setup moved to singleton, ad config as jsonfile.

--- model/index.js
+++ model/index.js
@@ -7,7 +7,7 @@ let filename = path.join(__dirname, "..", "settings", "db.json");
 let config = JSON.parse(fs.readFileSync(filename, "utf8"));
 config.storage = config.storage && path.join(path.dirname(filename), config.storage);
 
-const sequelize = new Sequelize('', '', '', config);
+const sequelize = new Sequelize(config);
 
 sequelize.import('./item');
 sequelize.import('./itemType');
